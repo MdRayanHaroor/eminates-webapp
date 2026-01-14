b85bdfe0bd0c20e8f7cb147ac0226e522c64b42f
Haroor Mohamed Rayan <haroor.mohamed@datasemantics.co>
2026-01-14 19:32:14 +0530

    img transition changed to 0.5

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -69,7 +69,7 @@ const MainBanner = () => {
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
-                    transition={{ duration: 1 }}
+                    transition={{ duration: 0.5 }}
                     className="absolute inset-0 z-0"
                 >
                     <img
